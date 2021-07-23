c69bad382a8144a5df7981ae3fe788e813438bd4
aalouaoui <khalidaalouaoui@gmail.com>
2021-07-24 00:36:54 +0100

    Added Wallpaper back

--- rc.lua
+++ rc.lua
@@ -35,16 +35,28 @@ awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.floating
 }
--- }}}
 
--- Menubar configuration
--- }}}
+local function set_wallpaper(s)
+    -- Wallpaper
+    if beautiful.wallpaper then
+        local wallpaper = beautiful.wallpaper
+        -- If wallpaper is a function, call it with the screen
+        if type(wallpaper) == "function" then
+            wallpaper = wallpaper(s)
+        end
+        gears.wallpaper.maximized(wallpaper, s, true)
+    end
+end
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal("property::geometry", set_wallpaper)
 
 -- {{{ Wibar
 awful.screen.connect_for_each_screen(
     function(s)
         -- Each screen has its own tag table.
         awful.tag({"1", "2", "3", "4", "5", "6", "7", "8", "9"}, s, awful.layout.layouts[1])
+        set_wallpaper(s)
         top_panel.create(s)
     end
 )
