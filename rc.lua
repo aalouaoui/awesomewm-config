6e844e1bdeea3cdf57efe9c707ffeeba7efc5d65
aalouaoui <khalidaalouaoui@gmail.com>
2021-07-23 23:11:44 +0100

    Combine Tag list

--- rc.lua
+++ rc.lua
@@ -90,54 +90,6 @@ awful.screen.connect_for_each_screen(
                 )
             )
         )
-        -- Create a taglist widget
-        s.mytaglist =
-            awful.widget.taglist {
-            screen = s,
-            filter = awful.widget.taglist.filter.all,
-            buttons = gears.table.join(
-                awful.button(
-                    {},
-                    1,
-                    function(t)
-                        t:view_only()
-                    end
-                ),
-                awful.button(
-                    {modkey},
-                    1,
-                    function(t)
-                        if client.focus then
-                            client.focus:move_to_tag(t)
-                        end
-                    end
-                ),
-                awful.button({}, 3, awful.tag.viewtoggle),
-                awful.button(
-                    {modkey},
-                    3,
-                    function(t)
-                        if client.focus then
-                            client.focus:toggle_tag(t)
-                        end
-                    end
-                ),
-                awful.button(
-                    {},
-                    4,
-                    function(t)
-                        awful.tag.viewprev(t.screen)
-                    end
-                ),
-                awful.button(
-                    {},
-                    5,
-                    function(t)
-                        awful.tag.viewnext(t.screen)
-                    end
-                )
-            )
-        }
 
         -- Create the wibox
         s.mywibox = awful.wibar({position = "top", screen = s})
@@ -148,7 +100,52 @@ awful.screen.connect_for_each_screen(
             {
                 -- Left widgets
                 layout = wibox.layout.fixed.horizontal,
-                s.mytaglist
+                awful.widget.taglist {
+                    screen = s,
+                    filter = awful.widget.taglist.filter.all,
+                    buttons = gears.table.join(
+                        awful.button(
+                            {},
+                            1,
+                            function(t)
+                                t:view_only()
+                            end
+                        ),
+                        awful.button(
+                            {modkey},
+                            1,
+                            function(t)
+                                if client.focus then
+                                    client.focus:move_to_tag(t)
+                                end
+                            end
+                        ),
+                        awful.button({}, 3, awful.tag.viewtoggle),
+                        awful.button(
+                            {modkey},
+                            3,
+                            function(t)
+                                if client.focus then
+                                    client.focus:toggle_tag(t)
+                                end
+                            end
+                        ),
+                        awful.button(
+                            {},
+                            4,
+                            function(t)
+                                awful.tag.viewprev(t.screen)
+                            end
+                        ),
+                        awful.button(
+                            {},
+                            5,
+                            function(t)
+                                awful.tag.viewnext(t.screen)
+                            end
+                        )
+                    )
+                }
             },
             {
                 -- Middle widget
