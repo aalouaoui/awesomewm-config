63afa2c8fb7df67e7d2c6d951d21134bc41e05ce
aalouaoui <khalidaalouaoui@gmail.com>
2021-07-23 19:22:48 +0100

    Joined wibox widgets

--- rc.lua
+++ rc.lua
@@ -46,126 +46,13 @@ awful.layout.layouts = {
 -- }}}
 
 -- {{{ Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
-    {
-        "hotkeys",
-        function()
-            hotkeys_popup.show_help(nil, awful.screen.focused())
-        end
-    },
-    {"manual", terminal .. " -e man awesome"},
-    {"edit config", editor_cmd .. " " .. awesome.conffile},
-    {"restart", awesome.restart},
-    {
-        "quit",
-        function()
-            awesome.quit()
-        end
-    }
-}
-
-mymainmenu =
-    awful.menu(
-    {
-        items = {
-            {"awesome", myawesomemenu},
-            {"open terminal", terminal}
-        }
-    }
-)
+mymainmenu = awful.menu({})
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
 
--- Keyboard map indicator and switcher
-
 -- {{{ Wibar
--- Create a textclock widget
-mytextclock = wibox.widget.textclock()
-
--- Create a wibox for each screen and add it
-local taglist_buttons =
-    gears.table.join(
-    awful.button(
-        {},
-        1,
-        function(t)
-            t:view_only()
-        end
-    ),
-    awful.button(
-        {modkey},
-        1,
-        function(t)
-            if client.focus then
-                client.focus:move_to_tag(t)
-            end
-        end
-    ),
-    awful.button({}, 3, awful.tag.viewtoggle),
-    awful.button(
-        {modkey},
-        3,
-        function(t)
-            if client.focus then
-                client.focus:toggle_tag(t)
-            end
-        end
-    ),
-    awful.button(
-        {},
-        4,
-        function(t)
-            awful.tag.viewprev(t.screen)
-        end
-    ),
-    awful.button(
-        {},
-        5,
-        function(t)
-            awful.tag.viewnext(t.screen)
-        end
-    )
-)
-
-local tasklist_buttons =
-    gears.table.join(
-    awful.button(
-        {},
-        1,
-        function(c)
-            if c == client.focus then
-                c.minimized = true
-            else
-                c:emit_signal("request::activate", "tasklist", {raise = true})
-            end
-        end
-    ),
-    awful.button(
-        {},
-        3,
-        function()
-            awful.menu.client_list({theme = {width = 250}})
-        end
-    ),
-    awful.button(
-        {},
-        4,
-        function()
-            awful.client.focus.byidx(1)
-        end
-    ),
-    awful.button(
-        {},
-        5,
-        function()
-            awful.client.focus.byidx(-1)
-        end
-    )
-)
-
 awful.screen.connect_for_each_screen(
     function(s)
         -- Each screen has its own tag table.
@@ -213,7 +100,48 @@ awful.screen.connect_for_each_screen(
             awful.widget.taglist {
             screen = s,
             filter = awful.widget.taglist.filter.all,
-            buttons = taglist_buttons
+            buttons = gears.table.join(
+                awful.button(
+                    {},
+                    1,
+                    function(t)
+                        t:view_only()
+                    end
+                ),
+                awful.button(
+                    {modkey},
+                    1,
+                    function(t)
+                        if client.focus then
+                            client.focus:move_to_tag(t)
+                        end
+                    end
+                ),
+                awful.button({}, 3, awful.tag.viewtoggle),
+                awful.button(
+                    {modkey},
+                    3,
+                    function(t)
+                        if client.focus then
+                            client.focus:toggle_tag(t)
+                        end
+                    end
+                ),
+                awful.button(
+                    {},
+                    4,
+                    function(t)
+                        awful.tag.viewprev(t.screen)
+                    end
+                ),
+                awful.button(
+                    {},
+                    5,
+                    function(t)
+                        awful.tag.viewnext(t.screen)
+                    end
+                )
+            )
         }
 
         -- Create a tasklist widget
@@ -221,7 +149,40 @@ awful.screen.connect_for_each_screen(
             awful.widget.tasklist {
             screen = s,
             filter = awful.widget.tasklist.filter.currenttags,
-            buttons = tasklist_buttons
+            buttons = gears.table.join(
+                awful.button(
+                    {},
+                    1,
+                    function(c)
+                        if c == client.focus then
+                            c.minimized = true
+                        else
+                            c:emit_signal("request::activate", "tasklist", {raise = true})
+                        end
+                    end
+                ),
+                awful.button(
+                    {},
+                    3,
+                    function()
+                        awful.menu.client_list({theme = {width = 250}})
+                    end
+                ),
+                awful.button(
+                    {},
+                    4,
+                    function()
+                        awful.client.focus.byidx(1)
+                    end
+                ),
+                awful.button(
+                    {},
+                    5,
+                    function()
+                        awful.client.focus.byidx(-1)
+                    end
+                )
+            )
         }
 
         -- Create the wibox
@@ -237,15 +198,16 @@ awful.screen.connect_for_each_screen(
                 s.mypromptbox
             },
             {
+                -- Middle widget
                 layout = wibox.layout.fixed.horizontal,
                 s.mytasklist
-            }, -- Middle widget
+            },
             {
                 -- Right widgets
                 layout = wibox.layout.fixed.horizontal,
                 awful.widget.keyboardlayout(),
                 wibox.widget.systray(),
-                mytextclock,
+                wibox.widget.textclock(),
                 s.mylayoutbox
             }
         }
