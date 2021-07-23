18370a8dc7e42640475315a488136915d7e23d39
aalouaoui <khalidaalouaoui@gmail.com>
2021-07-24 00:08:21 +0100

    Modified Keybindings

--- rc.lua
+++ rc.lua
@@ -24,13 +24,7 @@ beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
+filemanager = "thunar"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
