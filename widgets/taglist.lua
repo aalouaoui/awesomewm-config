1e42a139b0904a01a20e3b2ae14229e01e3bb3df
aalouaoui <khalidaalouaoui@gmail.com>
2021-07-24 20:25:55 +0100

    Refactor taglist

--- widgets/taglist.lua
+++ widgets/taglist.lua
@@ -5,50 +5,52 @@ local taglist = {}
 
 taglist.create = function(s)
     return awful.widget.taglist(
-        s,
-        awful.widget.taglist.filter.all,
-        gears.table.join(
-            awful.button(
-                {},
-                1,
-                function(t)
-                    t:view_only()
-                end
-            ),
-            awful.button(
-                {modkey},
-                1,
-                function(t)
-                    if client.focus then
-                        client.focus:move_to_tag(t)
+        {
+            screen = s,
+            filter = awful.widget.taglist.filter.all,
+            buttons = gears.table.join(
+                awful.button(
+                    {},
+                    1,
+                    function(t)
+                        t:view_only()
                     end
-                end
-            ),
-            awful.button({}, 3, awful.tag.viewtoggle),
-            awful.button(
-                {modkey},
-                3,
-                function(t)
-                    if client.focus then
-                        client.focus:toggle_tag(t)
+                ),
+                awful.button(
+                    {modkey},
+                    1,
+                    function(t)
+                        if client.focus then
+                            client.focus:move_to_tag(t)
+                        end
                     end
-                end
-            ),
-            awful.button(
-                {},
-                4,
-                function(t)
-                    awful.tag.viewprev(t.screen)
-                end
-            ),
-            awful.button(
-                {},
-                5,
-                function(t)
-                    awful.tag.viewnext(t.screen)
-                end
+                ),
+                awful.button({}, 3, awful.tag.viewtoggle),
+                awful.button(
+                    {modkey},
+                    3,
+                    function(t)
+                        if client.focus then
+                            client.focus:toggle_tag(t)
+                        end
+                    end
+                ),
+                awful.button(
+                    {},
+                    4,
+                    function(t)
+                        awful.tag.viewprev(t.screen)
+                    end
+                ),
+                awful.button(
+                    {},
+                    5,
+                    function(t)
+                        awful.tag.viewnext(t.screen)
+                    end
+                )
             )
-        )
+        }
     )
 end
 
