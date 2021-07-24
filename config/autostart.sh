1094cf9c615c918cbb22546790a3f09a851c5fb6
aalouaoui <khalidaalouaoui@gmail.com>
2021-07-24 18:57:02 +0100

    Restart picom if it crashes

--- config/autostart.sh
+++ config/autostart.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # If picom is running it means autostart script has already ran
 # and we don't need to rerun the same apps again
+if [[ -z $(pidof picom) ]]; then
+    picom --experimental-backends --config $HOME/.config/awesome/config/picom.conf &
+fi
+
 if [[ -n $(pidof nm-applet) ]]; then
     exit
 fi
 
-picom --experimental-backends --config $HOME/.config/awesome/config/picom.conf &
 nm-applet --indicator &
 xfce4-power-manager &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
