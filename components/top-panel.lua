e95a353f3fc99f048588551812560c03cc8d40d1
aalouaoui <khalidaalouaoui@gmail.com>
2021-07-24 19:43:41 +0100

    Added Pastel task-list

--- components/top-panel.lua
+++ components/top-panel.lua
@@ -65,44 +65,7 @@ top_panel.create = function(s)
         {
             -- Middle widget
             layout = wibox.layout.fixed.horizontal,
-            awful.widget.tasklist {
-                screen = s,
-                filter = awful.widget.tasklist.filter.currenttags,
-                buttons = gears.table.join(
-                    awful.button(
-                        {},
-                        1,
-                        function(c)
-                            if c == client.focus then
-                                c.minimized = true
-                            else
-                                c:emit_signal("request::activate", "tasklist", {raise = true})
-                            end
-                        end
-                    ),
-                    awful.button(
-                        {},
-                        3,
-                        function()
-                            awful.menu.client_list({theme = {width = 250}})
-                        end
-                    ),
-                    awful.button(
-                        {},
-                        4,
-                        function()
-                            awful.client.focus.byidx(1)
-                        end
-                    ),
-                    awful.button(
-                        {},
-                        5,
-                        function()
-                            awful.client.focus.byidx(-1)
-                        end
-                    )
-                )
-            }
+            require("widgets.task-list").create(s)
         },
         {
             -- Right widgets
